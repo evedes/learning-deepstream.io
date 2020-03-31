068da5616bf8a9bb0f08b701d59e71c7e3a9695a
Eduardo Vedes <eduardo.vedes@gmail.com>
2020-03-31 08:16:43 +0100

    baby steps into deepstream.io

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { DeepstreamClient } from "@deepstream/client";
 import "./App.css";
 
+const client = new DeepstreamClient("localhost:6020");
+client.login();
+
 const App = () => {
+  const record = client.record.getRecord("firstName");
+  record.subscribe("firstName", value => setValue(value));
+  const [value, setValue] = useState("");
+
+  console.log("state.value: ", value);
+  console.log("record: ", client.record.getRecord("firstName"));
+
+  const onChange = e => {
+    const { value } = e.target;
+    setValue(value);
+    record.set("firstName", value);
+  };
+
   return (
     <div className="App">
       <div>
         <h1>deepstream.io</h1>
       </div>
-      <div>Hey Joe</div>
+      <div>
+        <input value={value} onChange={onChange} />
+      </div>
     </div>
   );
 };
